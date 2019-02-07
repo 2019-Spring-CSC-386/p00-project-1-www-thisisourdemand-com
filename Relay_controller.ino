e99024a6717932c4530532129787686eb0ad25ba
Grantrd <31990206+Grantrd@users.noreply.github.com>
2019-02-06 20:38:01 -0500

    Mostly done with the code

--- Relay_controller.ino
+++ Relay_controller.ino
@@ -5,34 +5,84 @@
 int relay = 8;
 volatile byte relayState = LOW;
 int photocellpin = A2;
-int photocellread;
-int range = 400;
-//const int trigPin = 13;
-//const int echoPin = 11;
+const int trigPin = 13;
+const int echoPin = 11;
+bool var = false;
 
 void setup() {
   // Pin for relay module set as output
   pinMode(relay, OUTPUT);
   Serial.begin(9600);
 }
+void loop()
+{
+  power(relay, var);
+  while (motion(trigPin, echoPin))
+  {
+    if (dark(photocellpin))
+    {
+      power(relay, true);
+      delay(10000);
+      power(relay, false);
+      var = false;
+    }
+  }
+  motion(trigPin, echoPin);
+  dark(photocellpin);
+  power(relay, var);
 
-void loop() {
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
+}
+bool motion(int x, int y)
+{
+  pinMode(x, OUTPUT);
+  digitalWrite(x, LOW);
   delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
+  digitalWrite(x, HIGH);
   delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-
-  photocellread = analogRead(photocellpin);
-  Serial.print("Value: ");
-  Serial.println(photocellread);
-  if (photocellread < range) {
-    digitalWrite(relay, LOW);
-    Serial.println("on");
-  } else {
-    digitalWrite(relay, HIGH);
-    Serial.println("off");
+  digitalWrite(x, LOW);
+  pinMode(y, INPUT);
+  double duration = pulseIn(y, HIGH);
+  double inches = duration / 74 / 2;
+  Serial.print("distance: ");
+  Serial.println(inches);
+  if (inches < 120)
+  {
+    return true;
+    Serial.println("Distance true");
+  }
+  else
+  {
+  Serial.println("Distance false");
+  return false;
+  }
+}
+bool dark(int bright)
+{
+  int range = 400;
+  int photocellread = analogRead(bright);
+  if (photocellread < range)
+  {
+    return true;
+    Serial.println("light true");
     delay(100);
+  }
+  else
+  {
+    return false;
+    Serial.println("light false");
+    delay(100);
+  }
+}
+void power(int pin, boolean onOff)
+{
+  if (onOff)
+  {
+    digitalWrite(pin, LOW);
+    Serial.println("on");
+  }
+  else
+  {
+    digitalWrite(pin, HIGH);
+    Serial.println("off");
   }
 }
